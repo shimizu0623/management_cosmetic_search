47d45c5ec5c8a275d23be379956f0e8c1d09b54a
shimizu <yoonairi0623@gmail.com>
2022-07-06 05:18:18 +0900

    新規作成

---
+++ src/app/Http/Controllers/LeaveReasonController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LeaveReason;
+use Illuminate\Support\Facades\Hash;
+
+class LeaveReasonController extends Controller
+{
+    /**
+     * Display a listing of the users
+     *
+     * @param  \App\Models\User  $model
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        $leaveReasons = LeaveReason::all();
+        
+        return view('leaveReasons.index', ['leaveReasons' => $leaveReasons]);
+    }
+}
