33e968bd06d71bf12ce9c8d9937efd64a106dbe2
shimizu <yoonairi0623@gmail.com>
2022-11-30 13:26:18 +0900

    /skin_typesのroute追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -27,4 +27,5 @@ Auth::routes();
 Route::group(['middleware' => 'auth'], function () {
 	Route::get('/requests', [App\Http\Controllers\RequestController::class, 'index'])->name('requests');
 	Route::get('/leave_reasons', [App\Http\Controllers\LeaveReasonController::class, 'index'])->name('leaveReason');		
+	Route::get('/skin_types', [App\Http\Controllers\SkinTypeController::class, 'index'])->name('skinTypes');		
 });
